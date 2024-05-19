25495b6e9b3664f38a81ef468f73dfaff0ce2f9d
cristian alejandro <calejandro@macbook.pro>
2024-05-19 16:36:43 -0500

    Update version v1.0.3

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import { InfrastructureType } from "../types"
 
 export const configuration = {
-  name: 'rn',
+  name: 'rnca',
   version: '0.0.1',
   description: '✨ React Native Clean Architecture CLI ✨\n🛠️ CLI para crear el boilerplate de un proyecto en React Native ⚛️ con Clean Architecture. 📂 Incluye carpetas de dominio, aplicacion e infraestructura.'
 }
